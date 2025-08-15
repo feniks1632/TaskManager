c896ed539f08f22c46f39949e50b1f4a014cb5ec
feniks1632 <feniks1632@gmail.com>
2025-08-15 21:25:41 +0500

    creating_urls

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse  # ← добавь это!
 
 # Create your views here.
+def task_list(request):
+    return HttpResponse('Это список тасков')
+
+def task_create(request):
+    return HttpResponse('Это создание таски')
+
+def task_update(request, pk):
+    return HttpResponse('это обновление таски')
+
+def task_delete(request, pk):
+    return HttpResponse('это удаление таски')
